08e6393beb1b05a57dae2080964aa8079eb25f3f
adeel <adeel.bandesha148@gmail.com>
2024-06-04 16:37:20 +0500

    first commit

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    start_date = models.DateField()
+    responsible = models.ForeignKey(User, on_delete=models.CASCADE)
+    week_number = models.CharField(max_length=2, blank=True)
+    end_date = models.DateField()
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        print(self.start_date.isocalendar()[1])
+        if self.week_number == "":
+            self.week_number = self.start_date.isocalendar()[1]
+        super().save(*args, **kwargs)
+
+
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
